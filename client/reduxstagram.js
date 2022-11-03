645c46c1906c1ed72aa3ba2230e7569954ee6ab9
Michael Bevilacqua <mebevilacqua@hotmail.com>
2022-11-03 10:40:35 -0600

    Basic setup

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,8 @@
 // let's go!
+import React from 'react'
+import { render } from 'react-dom'
+
+// import css
+import css from './styles/style.styl'
+
+render(<p>hello</p>, document.getElementById('root'))
