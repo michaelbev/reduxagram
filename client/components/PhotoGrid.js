d3ae838d7f4913a3449ec9f47fafa22fa5597ce4
Michael Bevilacqua <mebevilacqua@hotmail.com>
2022-11-03 21:13:09 -0600

    Create initial store

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div className="photo-grid">
+        <h1>
+          <p>Im photo-grid</p>
+        </h1>
+      </div>
+    )
+  },
+})
+
+export default PhotoGrid
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,4 +5,18 @@ import { render } from 'react-dom'
 // import css
 import css from './styles/style.styl'
 
-render(<p>hello</p>, document.getElementById('root'))
+import Main from './components/Main'
+import Single from './components/Single'
+import PhotoGrid from './components/PhotoGrid'
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+render(router, document.getElementById('root'))
